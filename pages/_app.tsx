d68f01491c2dae803fef226573e458e71872a17f
RickyPox <ricardojsribeiro99@gmail.com>
2024-08-13 20:22:35 +0100

    Metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta name="description" content="Accelerating, Growing and Activating your favourite Web3 projects." />
+        <meta property="og:title" content="SkyRiseLabs" />
+        <meta property="og:description" content="Accelerating, Growing and Activating your favourite Web3 projects." />
+        <meta property="og:image" content="/favicon.png" />
+      </Head>
+    <Component {...pageProps}/>
+    </div>
+  ) 
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,7 @@ export default function Home() {
       <Head>
         <title>SkyRise</title>
         <link rel="icon" type="image/x-icon" href="/favicon.png"/>
-        <meta name="description" content="Accelerating, Growing and Activating your favourite Web3 projects." />
-        <meta property="og:title" content="SkyRiseLabs" />
-        <meta property="og:description" content="Accelerating, Growing and Activating your favourite Web3 projects." />
-        <meta property="og:image" content="/favicon.png" />
+
       </Head>
       <LoadingProvider>
         <LoadingVideo></LoadingVideo>
