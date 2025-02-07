8b246cc0078a64bf0acd573e64dd32938dbeb051
Ricky <ricardojsribeiro99@gmail.com>
2025-02-07 12:22:13 +0000

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
+      
+
   <title>SkyRiseLabs</title>
   <meta name="description" content="Leading Solana & Web3 growth agency driving mainstream adoption through strategic marketing, brand development, and event activation. Turn your vision into reality." />
   
+  {/* Google Analytics */}
+  <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JM8WE01GDG"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JM8WE01GDG');
+        `}
+      </Script>
+
   {/* Open Graph / Facebook / Discord */}
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://skyriselabs.io/" />
