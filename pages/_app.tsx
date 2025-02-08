d4fbfaaae613a56c23d866c59786e0b74f91a384
Ricky <ricardojsribeiro99@gmail.com>
2025-02-08 19:41:33 +0000

    Metatag zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         `}
       </Script>
 
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
 
 
   {/* Open Graph / Facebook / Discord */}
