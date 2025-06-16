257fe5e48fe6e016b353c63f014a0238755daa6b
Ricky <ricardojsribeiro99@gmail.com>
2025-06-16 17:05:33 +0100

    Add Videos On Studios / Remove Skyrise Studios from nav

---
+++ arrays/studios.js
@@ -0,0 +1,64 @@
+const studio = [
+    {
+        title: "Magic Eden Off-Site | 2024",
+        img: "Magic_Eden.png",
+        video: "https://www.youtube.com/embed/p0mQ1PKX1nQ?si=JFHDTJZOW-m9HmXt",
+        categorie: "Off-Site Coverage",
+    },
+    {
+        title: "Mad Lads x Backpack Party, Bangkok | 2024",
+        img: "MadLads_ Backpack_Party_Bangkok.png",
+        video: "https://www.youtube.com/embed/VEJRxkTwOkw?si=pADs59uvKwO72ha8",
+        categorie: "Party Coverage",
+    },
+    {
+        title: "mtnDAO v7 | Week 1 Recap | Winter 2025",
+        img: "mtnDAO_v7_Week_1_Recap.png",
+        video: "https://www.youtube.com/embed/9IKwVHN6xfw?si=p0mU78UA41ANAA-3",
+        categorie: "Weekly Video Recap",
+    },
+    {
+        title: "Pyra",
+        img: "Pyra.png",
+        video: "https://www.youtube.com/embed/Li7DQW9tlHg?si=jzeQPRvga2u3RCkD",
+        categorie: "Product Advertisement",
+    },
+    {
+        title: "IslandDAO v1, Crete | Recap #4 | 2024",
+        img: "islandDAO_v1_crete_week4.png",
+        video: "https://www.youtube.com/embed/0HWaXwpBurE?si=NseSZd6TDuKkzWem",
+        categorie: "Weekly Video Recap",
+    },
+    {
+        title: "Moonwalk | Hacker House, London | 2024",
+        img: "Moonwalk_HackerHouse_London.png",
+        video: "https://www.youtube.com/embed/R2LgORJWAog?si=6d4mf1i9dbMWeRQO",
+        categorie: "Product Activation",
+    },
+    {
+        title: "TidalDAO x Drift Mixer, Salt Lake City | 2025",
+        img: "tidalDAO_Drift.png",
+        video: "https://www.youtube.com/embed/kS5HRGcevlE?si=7mtgIffAoZj5dDyN",
+        categorie: "Party Coverage",
+    },
+    {
+        title: "mtnDAO v7 | Week 4 Recap | Winter 2025",
+        img: "mtnDAO_v7_Week_4_Recap.png",
+        video: "https://www.youtube.com/embed/0woFt-Ua7Yo?si=aTpOtNZ4WMbsOOu1",
+        categorie: "Weekly Video Recap",
+    },
+    {
+        title: "Amy Street x Kraken Exchange",
+        img: "Amy_Street_Kraken_Exchange.png",
+        video: "https://www.youtube.com/embed/CaYeHI_PnYs?si=9LB6U1i3hZvgrPfn",
+        categorie: "Brand Advertisement",
+    },
+    {
+        title: "Moonwalk | mtndao Hike, Salt Lake City | Summer 2024",
+        img: "Moonwalk_MtnDAO.png",
+        video: "https://www.youtube.com/embed/O-2hKYTifUg?si=ygO3uNqqVcqSBjAB",
+        categorie: "Product Activation",
+    },
+];
+
+export default studio;
